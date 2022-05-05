243c8e50ce3663daf5165f547071628eaaf6a3ec
dragonyu1010 <provic0099@gmail.com>
2022-05-05 23:44:41 +0800

    replace built-in getDateName() method with imported getDateName()

--- api/controllers/nftController.js
+++ api/controllers/nftController.js
@@ -30,22 +30,6 @@ async function mint(hash) {
 	return tx
 }
 
-// function getDateName() {
-// 	let curDate = new Date()
-// 	let year = curDate.getFullYear().toString()
-// 	let month = (curDate.getMonth()+1).toString()
-// 	if(curDate.getMonth() < 9 ) month = '0' + month;
-// 	let date = curDate.getDate().toString() 
-// 	if(curDate.getDate() < 10 ) date = '0' + date;
-// 	let hour = curDate.getHours().toString()
-// 	if(hour < 10 ) hour = '0' + hour;
-// 	let min = curDate.getMinutes().toString() 
-// 	if(curDate.getMinutes() < 10) min = '0' + min;
-// 	let sec = curDate.getSeconds().toString()
-// 	if(curDate.getSeconds() < 10 ) sec = '0' + sec;
-// 	return (year + month + date + hour + min + sec)
-// }
-// https://gateway.pinata.cloud/ipfs/QmPAd7oqiiCqi7Z6LRWzaz8vhZeJT4jxmckWWeXydJsQwu
 exports.createNft = async function(req, res) {
 	let fileName = req.body.title + '_' + getDateName() + '.jpeg'
 	let filePath = process.env.PWD + '/files/' + fileName 
